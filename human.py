82baecf683df247c58be2abdd46c562804b6257c
JJC <jjc@nlplab.cc>
2018-05-01 19:16:12 +0800

    finish human.py

--- human.py
+++ human.py
@@ -2,5 +2,32 @@
 
 
 class Human:
+    def __init__(self, name):
+        self.name = name
+
     def say_hi(self):
         print(self.name, 'says', self.hi)
+
+
+class American(Human):
+    hi = 'Hello'
+
+
+class French(Human):
+    hi = 'Bonjour'
+
+
+class Spanish(Human):
+    hi = 'Hola'
+
+
+class Japanese(Human):
+    hi = 'Konnichiwa'
+
+
+class Korean(Human):
+    hi = 'Yeoboseyo'
+
+
+class Indian(Human):
+    hi = 'Namaste'
